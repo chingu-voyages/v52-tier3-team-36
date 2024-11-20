56e8210f46d4968e8bf56d611cce8318d023d59c
Sunny Master <master.codeworks@gmail.com>
2024-11-20 15:39:02 -0500

    create frontend using React vite template

---
+++ DayCareApp/frontend/src/pages/Landing/Landing.jsx
@@ -0,0 +1,12 @@
+// css
+import styles from './Landing.module.css'
+
+const Landing = () => {
+  return (
+    <main className={styles.container}>
+      <h1>Kinderly</h1>
+    </main>
+  )
+}
+
+export default Landing
