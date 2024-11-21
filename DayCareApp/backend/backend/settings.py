5974a5cdc1024f71ee593deeda9dc53f83eef91c
rumenji <rumen_ji@yahoo.com>
2024-11-21 12:10:13 -0500

    Allow all origins in CORS
    Allow all origins in CORS for testing

--- DayCareApp/backend/backend/settings.py
+++ DayCareApp/backend/backend/settings.py
@@ -63,12 +63,13 @@ MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    '*',
-    # "http://localhost:3000",
-    # "http://127.0.0.1:3000",
-]
+# CORS_ALLOWED_ORIGINS = [
+#     '*',
+#     # "http://localhost:3000",
+#     # "http://127.0.0.1:3000",
+# ]
 
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'backend.urls'
