e29920b02be459f35ec2be8e8c5cd7163884f7e0
rumenji <rumen_ji@yahoo.com>
2024-12-15 10:57:37 -0500

    Update settings.py

--- DayCareApp/backend/backend/settings.py
+++ DayCareApp/backend/backend/settings.py
@@ -166,7 +166,7 @@ STORAGES = {
         "BACKEND": 'cloudinary_storage.storage.MediaCloudinaryStorage',
     },
     "staticfiles": {
-        "BACKEND": 'whitenoise.storage.CompressedManifestStaticFilesStorage',
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
     },
 }
 
@@ -179,12 +179,12 @@ CLOUDINARY_STORAGE = {
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 # This production code might break development mode, so we check whether we're in DEBUG mode
-# if not DEBUG:
+if not DEBUG:
     # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
     # and renames the files with unique names for each version to support long-term caching
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
