07b5aa19761f2f3f77ba3c814c64291311ce3665
rumenji <rumen_ji@yahoo.com>
2024-12-15 12:55:26 -0500

    Update settings.py

--- DayCareApp/backend/backend/settings.py
+++ DayCareApp/backend/backend/settings.py
@@ -63,11 +63,11 @@ MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    'https://kinderly-frontend.onrender.com'
-]
+# CORS_ALLOWED_ORIGINS = [
+#     'https://kinderly-frontend.onrender.com'
+# ]
 
-# CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'backend.urls'
