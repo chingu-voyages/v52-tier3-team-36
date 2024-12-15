f1bd9784f26a5328b5b5b7c7b7e80f80c2ade7dc
rumenji <rumen_ji@yahoo.com>
2024-12-15 10:44:29 -0500

    fix\cloudinary breaks the app
    fix\cloudinary breaks the app

--- DayCareApp/backend/backend/settings.py
+++ DayCareApp/backend/backend/settings.py
@@ -101,18 +101,8 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-# 'default': {
-# 'ENGINE': 'django.db.backends.postgresql',
-# 'NAME': 'daycare',
-# 'HOST': 'localhost',
-# 'PORT': '5432', # default PostgreSQL port
-# }
-# }
 DATABASES = {
     'default': dj_database_url.config(
-        # Replace this value with your local database's connection string.
-        # default='postgresql://postgres:postgres@localhost:5432/mysite',
         default=DB_URL,
         conn_max_age=600
     )
@@ -171,14 +161,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STORAGES = {
-    "default": {
-        "BACKEND": 'cloudinary_storage.storage.MediaCloudinaryStorage',
-    },
-    "staticfiles": {
-        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
-    },
-}
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.RawMediaCloudinaryStorage'
 
 # Configure Cloudinary settings
 CLOUDINARY_STORAGE = {
